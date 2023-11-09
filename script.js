83461d5f4fa67351d887d0275f97e4a3d6687fee
dschattenberg <150327477+dschattenberg@users.noreply.github.com>
2023-11-09 08:42:26 +0100

    test
    test

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitForm(event) {
+    event.preventDefault(); // Prevents the default form submission behavior
+
+    // You can add additional logic here to handle the form submission, such as sending data to a server
+
+    alert('Form submitted!'); // For demonstration purposes, replace this with your own logic
+}
