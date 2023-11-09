cb65e2c2a7cab9d0570d8b9904772418daab6e77
dschattenberg <150327477+dschattenberg@users.noreply.github.com>
2023-11-09 11:26:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const internalDatabase = [
+    { id: 1, number: 123, description: 'First number' },
+    { id: 2, number: 456, description: 'Second number' },
+    // Add more entries as needed
+];
+
+function searchNumbers(event) {
+    event.preventDefault();
+
+    const numberQuery = parseInt(document.getElementById('numberQuery').value);
+
+    const results = internalDatabase.filter(entry => entry.number === numberQuery);
+
+    displayResults(results);
+}
+
+function displayResults(results) {
+    const resultsContainer = document.getElementById('searchResults');
+
+    if (results.length > 0) {
+        resultsContainer.innerHTML = '';
+
+        results.forEach(result => {
+            const resultElement = document.createElement('div');
+            resultElement.innerHTML = `<h3>${result.number}</h3><p>${result.description}</p>`;
+            resultsContainer.appendChild(resultElement);
+        });
+    } else {
+        resultsContainer.innerHTML = '<p>No results found.</p>';
+    }
+}
+
 function submitForm(event) {
     event.preventDefault(); // Prevents the default form submission behavior
 
