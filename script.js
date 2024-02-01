734018556617c4bfdea9ee7978b860375dfdb8a0
dschattenberg <150327477+dschattenberg@users.noreply.github.com>
2024-02-01 21:43:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,49 @@
-const internalDatabase = [
-    { id: 1, number: 123, description: 'anonymous card' },
-    { id: 2, number: 456, description: 'anonymous card' },
-     { id: 3, number: 999, description: 'anonymous card' },
-     { id: 4, number: 888, description: 'anonymous card' },
-     { id: 5, number: 666, description: 'anonymous card' },
-     { id: 6, number: 777, description: 'anonymous card' },
-     { id: 7, number: 444, description: 'anonymous card' },
-    // Add more entries as needed
-];
-
-function searchNumbers(event) {
-    event.preventDefault();
-
-    const numberQuery = parseInt(document.getElementById('numberQuery').value);
-
-    const results = internalDatabase.filter(entry => entry.number === numberQuery);
-
-    displayResults(results);
+body {
+    font-family: 'Arial', sans-serif;
+    margin: 0;
+    padding: 0;
 }
 
-    function copyNumberToCard(event) {
-    const numberQuery = document.getElementById('numberQuery');
-    const cardNumberField = document.getElementById('cardNumberField');
-
-    cardNumberField.value = numberQuery.value;
+header, nav, main, footer {
+    margin: 20px;
 }
 
-function displayResults(results) {
-    const resultsContainer = document.getElementById('searchResults');
-
-    if (results.length > 0) {
-        resultsContainer.innerHTML = '';
-
-        results.forEach(result => {
-            const resultElement = document.createElement('div');
-            resultElement.innerHTML = `<h3>${result.number}</h3><p>${result.description}</p>`;
-            resultsContainer.appendChild(resultElement);
-        });
-    } else {
-        resultsContainer.innerHTML = '<p>No Air Miles card found.</p>';
-    }
+header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
 }
 
-function submitForm(event) {
-    event.preventDefault(); // Prevents the default form submission behavior
-
-    // You can add additional logic here to handle the form submission, such as sending data to a server
-
-    alert('Form submitted!'); // For demonstration purposes, replace this with your own logic
-
+nav ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
 }
 
+nav a {
+    text-decoration: none;
+    color: #333;
+    padding: 5px 10px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    margin: 5px;
+    border-radius: 5px;
+}
 
+nav a:hover {
+    background-color: #eee;
+}
 
+main {
+    max-width: 800px;
+}
+
+section {
+    margin-bottom: 20px;
+}
+
+footer {
+    margin-top: 20px;
+    text-align: center;
+}
